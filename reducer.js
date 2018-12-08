522c1a5c055833349a1e603f9177d3979d6865fb
Dheeraj Suthar <dheersuthar@gmail.com>
2018-12-09 00:29:23 +0530

    Separate reducers using Object.assign.

--- reducer.js
+++ reducer.js
@@ -3,32 +3,50 @@ import {
     TOGGLE_TODO,
     SET_VISIBILITY_FILTER,
     VisibilityFilters,
-    setVisibilityFilter
+    addTodo,
 } from './actions';
-function todoApp(state = initialState, action) {
+import { stat } from 'fs';
+const { SHOW_ALL } = VisibilityFilters;
+
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+function todos(state = [], action) {
     switch (action.type) {
         case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text,
-                        completed: false
-                    }
-                ]
-            }
-            );
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ];
         case TOGGLE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map((todo, index) => {
-                    if (action.index === index) {
-                        return Object.assign({}, todo, { completed: !todo.completed });
-                    }
-                    return todo;
-                })
+            return state.map((todo, index) => {
+                if (action.index === index) {
+                    return Object.assign({}, todo, { completed: !todo.completed });
+                }
+                return todo;
             });
+        default:
+            return state;
+    }
+}
+
+export default function todoApp(state = {}, action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return Object.assign({}, state, { todos: todos(state.todos, action) });
+        case TOGGLE_TODO:
+            return Object.assign({}, state, { todos: todos(state.todos, action) });
 
         case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, { filter: action.filter });
+            return Object.assign({}, state, { filter: visibilityFilter(state.filter, action) });
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
-module: {
-    rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    mode: 'development',
+    module: {
+
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
-        }
-    ]
+        ]
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 }
